87b4fc65f8ec1117eb28666018d79ec2a1a2e7ee
ertugrulcetin <ertu.ctn@gmail.com>
2025-12-19 23:53:06 +0300

    Remove online components

--- sh/upload_editor_to_cf.sh
+++ sh/upload_editor_to_cf.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-rm -rf /Users/ertugrul/IdeaProjects/Immersa/resources/public/js/compiled
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+PROJECT_ROOT="$(cd "${SCRIPT_DIR}/.." && pwd)"
+
+rm -rf "${PROJECT_ROOT}/resources/public/js/compiled"
 
 current_timestamp=$(date +%s)000
 
-INDEX_PATH='/Users/ertugrul/IdeaProjects/Immersa/resources/public/index.html'
-ASSET_PATH='/Users/ertugrul/IdeaProjects/Immersa/resources/public'
+INDEX_PATH="${PROJECT_ROOT}/resources/public/index.html"
+ASSET_PATH="${PROJECT_ROOT}/resources/public"
 
 echo "Replacing __COMMIT_SHA__ with current timestamp"
 sed -i '' "s/__COMMIT_SHA__/${current_timestamp}/g" ${INDEX_PATH}
